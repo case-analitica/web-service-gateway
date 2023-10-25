c146bb7d8e12e028693c7e194c2e3ba0a12b4673
rfelipesp <rfelipesp@gmail.com>
2023-10-25 19:47:06 -0300

    Add CustomerDevice

--- src/main/java/com/caseanalitica/webservicegateway/app/rest/device/DeviceApi.java
+++ src/main/java/com/caseanalitica/webservicegateway/app/rest/device/DeviceApi.java
@@ -4,10 +4,7 @@ import com.caseanalitica.commons.ApiResponse;
 import com.caseanalitica.webservicegateway.infra.gateway.DeviceGateway;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
