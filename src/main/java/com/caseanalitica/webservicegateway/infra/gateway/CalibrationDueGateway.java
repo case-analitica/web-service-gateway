1932ff318b92995c751ffebc89cc337aa27c7308
rfelipesp <rfelipesp@gmail.com>
2024-02-05 21:04:51 -0300

    Adjust flow to CalibrationControlApi

--- src/main/java/com/caseanalitica/webservicegateway/infra/gateway/CalibrationDueGateway.java
+++ src/main/java/com/caseanalitica/webservicegateway/infra/gateway/CalibrationDueGateway.java
@@ -1,16 +1,10 @@
 package com.caseanalitica.webservicegateway.infra.gateway;
 
 import com.caseanalitica.commons.ApiResponse;
-import com.caseanalitica.webservicegateway.app.dto.calibration.CalibrationControl;
 import com.caseanalitica.webservicegateway.app.dto.calibration.CalibrationDue;
-import com.caseanalitica.webservicegateway.app.dto.calibration.CalibrationProgram;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @FeignClient(name = "calibration-due-api", url = "${api.url.calibration}")
 public interface CalibrationDueGateway {
